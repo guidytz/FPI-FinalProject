a9e07653c70555a3ec75acc9998492a39159a114
alexandre-da-costa <alexandredacosta@msn.com>
2019-11-10 15:26:51 -0300

    Update RF.py

--- RF.py
+++ RF.py
@@ -1,6 +1,23 @@
 import numpy as np
 import cv2
 
+## print formatado semelhante ao MATLAB para comparação de resultados
+def printMat (arr):
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            print("%+.4f" % arr[i][j][0], end = ' ')
+        print('')
+    print('')
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            print("%+.4f" % arr[i][j][1], end = ' ')
+        print('')
+    print('')
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            print("%+.4f" % arr[i][j][2], end = ' ')
+        print('')
+
 img = cv2.imread('statue_very_small.png')
 
 alt = np.array(img).shape[0]
@@ -12,21 +29,6 @@ img_norm = img/255
 dIcdy = np.diff(img_norm, 1, 1)
 dIcdy = np.diff(img_norm, 1, 0)
 
-print(np.array(dIcdy).shape)
+printMat(dIcdy)
 
 
-## print formatado semelhante ao MATLAB para comparação de resultados
-for i in range(len(dIcdy)):
-    for j in range(len(dIcdy[i])):
-        print("%.4f" % dIcdy[i][j][0], end = ' ')
-    print('')
-print('')
-for i in range(len(dIcdy)):
-    for j in range(len(dIcdy[i])):
-        print("%.4f" % dIcdy[i][j][1], end = ' ')
-    print('')
-print('')
-for i in range(len(dIcdy)):
-    for j in range(len(dIcdy[i])):
-        print("%.4f" % dIcdy[i][j][2], end = ' ')
-    print('')

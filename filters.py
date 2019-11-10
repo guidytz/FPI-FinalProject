94b73739717c6dd764d5f09ecae2531683e08b6a
Guilherme Dytz dos Santos <guidytz@gmail.com>
2019-11-10 00:20:31 -0300

    Adição do arquivo filter e modificação no main

---
+++ filters.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def RF(img, sigma_s, sigma_r, n_it=3, joint_img=None):
+    pass
+
+def NC(img, sigma_s, sigma_r, n_it=3, joint_img=None):
+    pass
+
+def IC(img, sigma_s, sigma_r, n_it=3, joint_img=None):
+    pass
--- main.py
+++ main.py
@@ -2,45 +2,6 @@ import sys, os
 import numpy as np
 import cv2
 
-# img = np.array(cv2.imread('lena.jpg', -1))
-# img = img / 255;
-# print(img)
-# k = cv2.waitKey(0)
-
-
-# if k == 27:
-#     cv2.destroyAllWindows()
-
-def RF(img, sigma_s, sigma_r, n_it=3, joint_img=None):
-    I = np.array(img)
-
-    if joint_img != None:
-        J = np.array(joint_img)
-        if I.shape[:2] != J.shape[:2]:
-            print("Input and joint images must have equal width and height!")
-            return   
-    else:
-        J = I
-
-    [h, w, n_channels] = J.shape
-
-    dIcdx = np.diff(J, 1, 2);
-    dIcdy = np.diff(J, 1, 1);
-    
-    dIdx = np.zeros((h, w), dtype=float);
-    dIdy = np.zeros((h, w), dtype=float);
-
-    # for c in range(n_channels):
-    #     dIdx[1:,:] = dIdx[:,:] + abs( dIcdx[:,:,c] )
-    #     # dIdy[:,1:] = dIdy[:,1:] + abs( dIcdy[:,:,c] )
-
-    # dHdx = (1 + sigma_s/sigma_r * dIdx)
-    # dVdy = (1 + sigma_s/sigma_r * dIdy)
-
-    dVdy = dVdy.transpose()
-
-    return
-
 if __name__ == "__main__":
     if len(sys.argv) != 2: 
         print("Proper usage: ")
@@ -56,11 +17,7 @@ if __name__ == "__main__":
         print("Nao foi possivel ler a imagem", "\'"+img_name+"\'")
         sys.exit()
 
-    RF(img, 0.1, 0.3)
-
     k = cv2.waitKey(0)
 
     if k == 27:
         cv2.destroyAllWindows()
-
-    pass

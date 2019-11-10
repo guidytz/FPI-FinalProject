ff0f40af0bcfd351933cb0bd0fe88cae7e566d1c
alexandre-da-costa <alexandredacosta@msn.com>
2019-11-10 19:12:49 -0300

    RF quase pronto

--- RF.py
+++ RF.py
@@ -5,96 +5,41 @@ import math
 
 img = cv2.imread('statue_very_small.png')
 
-sigma_s = 200
-sigma_r = 2
+sigs = 200
+sigr = 2
 it = 3
 
-[alt,lar,can]  = np.array(img).shape
+[alt, lar, can] = np.array(img).shape
 
-print(alt,lar,can)
+print(alt, lar, can)
 
 img_norm = img/255
 
-dIdx = np.zeros((alt,lar))
-dIdy = np.zeros((alt,lar))
+didx = np.zeros((alt, lar))
+didy = np.zeros((alt, lar))
 
-dIcdx = np.diff(img_norm, 1, 1)
+dicdx = np.diff(img_norm, 1, 1)
 dIcdy = np.diff(img_norm, 1, 0)
 
 for i in range(can):
-    dIdx[:,1:] = dIdx[:,1:] + abs(dIcdx[:,:,i])
-    dIdy[1:,:] = dIdy[1:,:] + abs(dIcdy[:,:,i])
+    didx[:, 1:] = didx[:, 1:] + abs(dicdx[:, :, i])
+    didy[1:, :] = didy[1:, :] + abs(dIcdy[:, :, i])
 
-dHdx = (1 + sigma_s/sigma_r * dIdx)
-dVdy = np.transpose(1 + sigma_s/sigma_r * dIdy)
+dhdx = (1 + sigs/sigr * didx)
+dvdy = np.transpose(1 + sigs/sigr * didy)
 
-sigma_H = sigma_s
+sigh = sigs
 
-for i in range(can):
-    sigma_H_i = sigma_H * math.sqrt(3) * (2**(it-(i+1))) / math.sqrt(4**it -1)
-    print(sigma_H_i)
+F = img_norm
+
+for i in range(it-1):
+    sigh_i = sigh * math.sqrt(3) * (2**(it-(i+1))) / math.sqrt(4**it - 1)
+    F = fn.rf(F,dhdx,sigh_i)
+    F = fn.transpose_image(F)
+    F = fn.rf(F,dvdy,sigh_i)
+    F = fn.transpose_image(F)
 
 
-"""     N = num_iterations;
-    F = I;
-    
-    sigma_H = sigma_s;
-    
-    for i = 0:num_iterations - 1
-    
-        % Compute the sigma value for this iteration (Equation 14 of our paper).
-        sigma_H_i = sigma_H * sqrt(3) * 2^(N - (i + 1)) / sqrt(4^N - 1);
-    
-        F = TransformedDomainRecursiveFilter_Horizontal(F, dHdx, sigma_H_i);
-        F = image_transpose(F);
-    
-        F = TransformedDomainRecursiveFilter_Horizontal(F, dVdy, sigma_H_i);
-        F = image_transpose(F);
-        
-    end
-    
-    F = cast(F, class(img));
+F = F*255
 
-end
-
-%% Recursive filter.
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function F = TransformedDomainRecursiveFilter_Horizontal(I, D, sigma)
-
-    % Feedback coefficient (Appendix of our paper).
-    a = exp(-sqrt(2) / sigma);
-    
-    F = I;
-    V = a.^D;
-    
-    [h w num_channels] = size(I);
-    
-    % Left -> Right filter.
-    for i = 2:w
-        for c = 1:num_channels
-            F(:,i,c) = F(:,i,c) + V(:,i) .* ( F(:,i - 1,c) - F(:,i,c) );
-        end
-    end
-    
-    % Right -> Left filter.
-    for i = w-1:-1:1
-        for c = 1:num_channels
-            F(:,i,c) = F(:,i,c) + V(:,i+1) .* ( F(:,i + 1,c) - F(:,i,c) );
-        end
-    end
-
-end
-
-%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function T = image_transpose(I)
-
-    [h w num_channels] = size(I);
-    
-    T = zeros([w h num_channels], class(I));
-    
-    for c = 1:num_channels
-        T(:,:,c) = I(:,:,c)';
-    end
-    
-end """
+# falta converter o F para NumPy array uint8
--- functions.py
+++ functions.py
@@ -1,16 +1,46 @@
-## print formatado semelhante ao MATLAB para comparação de resultados
-def printMat (arr):
+import math
+import numpy as np
+
+# print formatado semelhante ao MATLAB para comparaï¿½ï¿½o de resultados
+
+
+def printMat(arr):
     for i in range(len(arr)):
         for j in range(len(arr[i])):
-            print("%+.4f" % arr[i][j][0], end = ' ')
+            print("%+.4f" % arr[i][j][0], end=' ')
         print('')
     print('')
     for i in range(len(arr)):
         for j in range(len(arr[i])):
-            print("%+.4f" % arr[i][j][1], end = ' ')
+            print("%+.4f" % arr[i][j][1], end=' ')
         print('')
     print('')
     for i in range(len(arr)):
         for j in range(len(arr[i])):
-            print("%+.4f" % arr[i][j][2], end = ' ')
+            print("%+.4f" % arr[i][j][2], end=' ')
         print('')
+
+
+def rf(img, der, sigma):
+    a = math.exp(-math.sqrt(2) / sigma)
+    V = a ** der
+    F = img
+    [alt, lar, can] = np.array(img).shape
+
+    for i in range(1, lar):
+        for c in range(can):
+            F[:, i, c] = F[:, i, c] + V[:, i] * (F[:, i - 1, c] - F[:, i, c])
+
+    for i in range(lar-2, -1, -1):
+        for c in range(can):
+            F[:, i, c] = F[:, i, c] + V[:, i+1] * (F[:, i + 1, c] - F[:, i, c])
+
+    return F
+
+
+def transpose_image(img):
+    [h, w, num_channels] = np.array(img).shape
+    T = np.zeros((w, h, num_channels))
+    for c in range(num_channels):
+        T[:, :, c] = np.transpose(img[:, :, c])
+    return T

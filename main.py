47b0e1737c25b43cea8ba0e6f1a98a616af9593e
Guilherme Dytz dos Santos <guidytz@gmail.com>
2019-11-28 20:56:13 -0300

    Detail Enhancement Feature

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 """ Main file """
 import sys
 from time import perf_counter
-import cv2
+import cv2 as cv
 import functions as fn
+import numpy as np
+
+detail_enhancement = True
 
 if __name__ == "__main__":
     if len(sys.argv) != 6:
@@ -23,7 +26,10 @@ if __name__ == "__main__":
         sys.exit()
         
     # Read image
-    img = cv2.imread(img_name)
+    img = cv.imread(img_name)
+    if detail_enhancement:
+        img = cv.cvtColor(img, cv.COLOR_BGR2YCrCb)
+        D = np.array(img[:,:,0]).astype(float)
 
     try:
         # Start timer
@@ -31,26 +37,46 @@ if __name__ == "__main__":
         print("Start processing...")
 
         # Apply the filter
-        filtered_img = fn.ep_filter(img, filter_type, sigma_s, sigma_r, iterations)
+
+        if detail_enhancement:
+            filtered_img = fn.ep_filter(img, filter_type, sigma_s, sigma_r, iterations)
+            img_n = img / 255;
+            D[:,:] = img_n[:,:,0] - filtered_img[:,:,0]
+            D *= 2
+            img_n[:,:,0] += D[:,:]
+            filtered_img = cv.convertScaleAbs(img_n * 255)
+            filtered_img = cv.cvtColor(filtered_img, cv.COLOR_YCrCb2BGR)
+            img = cv.cvtColor(img, cv.COLOR_YCrCb2BGR)
+        else:
+            filtered_img = fn.ep_filter(img, filter_type, sigma_s, sigma_r, iterations)
+            filtered_img = cv.convertScaleAbs(filtered_img * 255)
 
         # Stop timer
         timer_stop = perf_counter()
 
         # Print time
         time = timer_stop-timer_start
-        print("Filter processing time: %.3f " % time, end="seconds.")
+        print("Filter processing time: %.3f " % time, end="seconds.\n")
         sys.stdout.flush()
 
+        
         # Show images
-        cv2.imshow('Original image', img)
-        cv2.imshow('Filtered image', filtered_img)
+        cv.imshow('Original image', img)
+        if detail_enhancement:
+            cv.imshow('Detail Enhanced', filtered_img)
+            new_img = 'results/' + str(img_name[:img_name.find('.')]) +'_'+ (filter_type) + '_DTE.png'
+        else:
+            cv.imshow('Filtered image', filtered_img)
+            new_img = 'results/' + str(img_name[:img_name.find('.')]) +'_'+ (filter_type) + '.png'
+
+        cv.imwrite(new_img, filtered_img)
     except:
         print("Error reading image or incorrect parameters for", "\'"+img_name+"\'")
         sys.exit()
 
-    k = cv2.waitKey(0)
+    k = cv.waitKey(0)
 
     if k == 27:
-        cv2.destroyAllWindows()
+        cv.destroyAllWindows()
 else:
     print("This module cannot be imported.")

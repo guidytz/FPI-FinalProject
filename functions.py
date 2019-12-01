ef8804065e104dc56a2a10ede81590fde5558624
alexandre-da-costa <alexandredacosta@msn.com>
2019-12-01 14:55:16 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -6,26 +6,6 @@ import numpy as np
 import filters as ft
 
 
-def print_3d(arr):
-    """Temporary function to print 3D arrays like in MATLAB. Remove before
-    submission."""
-
-    for i in range(len(arr)):
-        for j in range(len(arr[i])):
-            print("%+.4f" % arr[i][j][0], end=' ')
-        print('')
-    print('')
-    for i in range(len(arr)):
-        for j in range(len(arr[i])):
-            print("%+.4f" % arr[i][j][1], end=' ')
-        print('')
-    print('')
-    for i in range(len(arr)):
-        for j in range(len(arr[i])):
-            print("%+.4f" % arr[i][j][2], end=' ')
-        print('')
-
-
 def image_transpose(img):
     """Transposes an image in Numpy array
     Because we have 3 channels and we don't want to transpose then, this
